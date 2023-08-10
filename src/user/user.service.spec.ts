ec51961b98444b20ed3338a6c091204b0b585df6
sangyeopchae <sy.chae@teamsparta.co>
2023-08-10 12:23:18 +0900

    test: 로그인 관련 테스트 추가
    - 아이디/비밀번호 모두 올바를 경우
    - 아이디가 올바르지 않을 경우
    - 비밀번호가 올바르지 않을 경우

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,7 +3,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { RegisterUserDto } from './register-user.dto';
-import { BadRequestException } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -36,7 +40,7 @@ describe('UserService', () => {
 
   describe('회원 가입 테스트', () => {
     test('jwt 토큰이 잘 발급 되는지 테스트', async () => {
-      // given
+      //given
       const userId = userData.userId;
       const hashedPassword = await bcrypt.hash(userData.password, 10);
 
@@ -44,16 +48,16 @@ describe('UserService', () => {
         userId: userId,
         password: hashedPassword,
       });
-      // when
+      //when
       const result = await userService.register(userData);
-      // then
+      //then
       console.log(result);
       expect(result).toBeDefined();
       await userModelMock.deleteOne({ userId: userData.userId });
     });
 
     test('이미 존재하는 아이디가 아닐 경우, 회원가입이 잘 되는지 테스트', async () => {
-      // given
+      //given
       const userId = userData.userId;
       const hashedPassword = await bcrypt.hash(userData.password, 10);
 
@@ -63,13 +67,13 @@ describe('UserService', () => {
         password: hashedPassword,
       });
 
-      // when
+      //when
       const result = await userService.register(
         new RegisterUserDto(userData.userId, userData.password),
       );
       console.log(result);
 
-      // then
+      //then
       /* TODO: JWT 발급과 사용자 회원가입 메서드 분리해서 테스트 따로 만들기 */
       userModelMock.findOne.mockResolvedValue({
         userId: userId,
@@ -105,6 +109,55 @@ describe('UserService', () => {
           new RegisterUserDto(userData.userId, userData.password),
         );
       }).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('로그인 테스트', () => {
+    test('올바른 아이디와 패스워드를 입력했을 경우, 로그인이 잘 되는지 테스트', async () => {
+      //given
+      const userId = userData.userId;
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+      userModelMock.findOne.mockResolvedValue({
+        userId: userId,
+        password: hashedPassword,
+      });
+      //when
+      const result = await userService.login(
+        new RegisterUserDto(userData.userId, userData.password),
+      );
+      //then
+      expect(result).toBeDefined();
+    });
+
+    test('올바르지 않은 아이디를 입력했을 경우, 예외가 잘 발생하는지 테스트', async () => {
+      //given
+      userModelMock.findOne.mockResolvedValue(null);
+      //when
+      //then
+      await expect(async () => {
+        await userService.login(
+          new RegisterUserDto(userData.userId, userData.password),
+        );
+      }).rejects.toThrow(NotFoundException);
+    });
+
+    test('올바르지 않은 패스워드를 입력했을 경우, 예외가 잘 발생하는지 테스트', async () => {
+      //given
+      const userId = userData.userId;
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+      userModelMock.findOne.mockResolvedValue({
+        userId: userId,
+        password: hashedPassword,
+      });
+      //when
+      //then
+      await expect(async () => {
+        await userService.login(
+          new RegisterUserDto(userData.userId, 'wrongPassword'),
+        );
+      }).rejects.toThrow(UnauthorizedException);
     });
   });
 });
