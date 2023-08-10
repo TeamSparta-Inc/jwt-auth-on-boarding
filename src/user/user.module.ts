b00be3307606a64afc841e64a50d8859958b4a8c
sangyeopchae <sy.chae@teamsparta.co>
2023-08-10 22:12:47 +0900

    feat: 토큰 재발급 로직 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,14 @@ import { UserService } from './user.service';
 import { JwtService } from '../jwt/jwt.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
+import { RefreshToken, RefreshTokenSchema } from '../jwt/jwt.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService, JwtService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,17 +12,24 @@ import { JwtService } from '../jwt/jwt.service';
 import { User } from './user.schema';
 import { LoginUserDto } from './login-user.dto';
 import { InfoUserDto } from './info-user.dto';
+import { RefreshToken } from '../jwt/jwt.schema';
 
 @Injectable()
 export class UserService {
-  public static readonly INVALID_ID_MESSAGE = '아이디가 올바르지 않습니다';
-  public static readonly INVALID_PASSWORD_MESSAGE =
+  private static readonly INVALID_ID_MESSAGE = '아이디가 올바르지 않습니다';
+  private static readonly INVALID_PASSWORD_MESSAGE =
     '비밀번호가 올바르지 않습니다';
-  public static readonly DUPLICATED_ID_MESSAGE = '이미 존재하는 아이디 입니다.';
-
+  private static readonly DUPLICATED_ID_MESSAGE =
+    '이미 존재하는 아이디 입니다.';
+  private static readonly INVALID_REFRESH_TOKEN_MESSAGE =
+    '올바르지 않은 토큰입니다.';
   jwtService = new JwtService();
 
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    @InjectModel(RefreshToken.name)
+    private refreshTokenModel: Model<RefreshToken>,
+  ) {}
 
   async register(registerUserDto: {
     password: string;
@@ -41,7 +48,14 @@ export class UserService {
       password: hashedPassword,
     });
 
-    return this.jwtService.publishToken(savedUser._id);
+    const tokens = this.jwtService.publishToken(savedUser._id);
+    await this.refreshTokenModel.create({
+      refreshToken: tokens.refreshToken,
+      userId: savedUser._id,
+      createdAt: Date.now(),
+    });
+
+    return tokens;
   }
 
   async login(loginUserDto: LoginUserDto): Promise<TokenDto> {
@@ -56,6 +70,28 @@ export class UserService {
     }
 
     return this.jwtService.publishToken(user._id);
+  }
+
+  async refresh(refreshToken: string): Promise<TokenDto> {
+    const userId = this.jwtService.verifyToken(refreshToken);
+    const existsToken = await this.refreshTokenModel.findOne({
+      userId: userId,
+    });
+
+    if (!existsToken || existsToken.refreshToken !== refreshToken) {
+      throw new UnauthorizedException(
+        UserService.INVALID_REFRESH_TOKEN_MESSAGE,
+      );
+    }
+    const tokens = this.jwtService.publishToken(userId);
+
+    await this.refreshTokenModel.updateOne({
+      userId: userId,
+      refreshToken: tokens.refreshToken,
+      createdAt: Date.now(),
+    });
+
+    return tokens;
   }
 
   async getInfo(id: string): Promise<InfoUserDto> {
